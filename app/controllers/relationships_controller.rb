d87d29e1bc87322429dafc7f5528df87cf1cc857
truongthanh <truongthanh.bk56@gmail.com>
2017-05-25 14:27:08 +0700

    follow user

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,32 @@
+class RelationshipsController < ApplicationController
+  before_action :user_signed_in?
+
+  def index
+    @user = User.find_by id: params[:user_id]
+    @title = params[:type]
+    if %w(following followers).include? params[:type]
+      @users = @user.send(params[:type]).paginate page: params[:page],
+        per_page: Settings.users.per_page
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @user = User.find params[:followed_id]
+    current_user.follow @user
+    respond_to do |format|
+      format.html{redirect_to @user}
+      format.js
+    end
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow @user
+    respond_to do |format|
+      format.html{redirect_to @user}
+      format.js
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  get "users/new"
+
   root "pages#index"
 
   devise_for :users, class_name: "FormUser", controllers:
-    {omniauth_callbacks: "omniauth_callbacks", registrations: "registrations"}
+  {omniauth_callbacks: "omniauth_callbacks", registrations: "registrations"}
 
   get "/home", to: "pages#show", as: "home"
   get "/", to: "pages#index", as: "landing"
+  resources :users, only: [:index, :show] do
+  	resources :relationships, only: [:index]
+  end
+  resources :relationships, only: [:create, :destroy]
 end
