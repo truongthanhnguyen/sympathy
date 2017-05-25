c185fc95b749c4d3ec4a98f96addfeca69a98176
Kien Do <kiendo95@gmail.com>
2017-05-25 11:35:25 +0700

    Styling devise views

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationsController < Devise::RegistrationsController
+  def update_resource resource, params
+    if resource.encrypted_password.blank?
+      resource.email = params[:email] if params[:email]
+      if params[:password].present? &&
+        params[:password] == params[:password_confirmation]
+        resource.password = params[:password]
+        resource.save
+      end
+      resource.update_without_password params if resource.valid?
+    else
+      resource.update_with_password params
+    end
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find params[:id]
+    @relationship = current_user.active_relationships
+      .find_by followed_id: @user.id
   end
 end
